ebe7a20d7bea07a2fb0687c1423e5ef0cc4197f3
mahmudhmh <54104161+mahmudhmh@users.noreply.github.com>
2024-06-19 04:53:36 +0300

    fix[server]: fixed some issues with tsconfig.json

---
+++ crud-server/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js'],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+        '^.+\\.(t|j)sx?$': 'babel-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/$1',
+    },
+    watchman: false,
+};
+//# sourceMappingURL=jest.config.js.map
